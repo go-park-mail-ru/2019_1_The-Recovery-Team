21761a182ca56a50a9633995f2b47c2e303cae29
Arsenij Zorin <zorinarsenij@mail.ru>
2019-03-01 21:32:52 +0300

    [ADD] Create entry point

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"api/database"
+	"api/models"
+	"api/router"
+)
+
+func main() {
+	dbm, err := database.InitDatabaseManager("recoveryteam", "123456", "localhost", "sadislands")
+	if err != nil {
+		panic(err)
+	}
+
+	env := &models.Env{
+		Dbm: dbm,
+	}
+
+	router := router.InitRouter(env)
+	if err := http.ListenAndServe(":9090", router); err != nil {
+		panic(err)
+	}
+}
