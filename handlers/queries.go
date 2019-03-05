9a8574f08b3b4767e1094d4fe2475dbac555cf2e
Arsenij Zorin <zorinarsenij@mail.ru>
2019-03-05 13:41:13 +0300

    [ADD] New queries for profile

--- handlers/queries.go
+++ handlers/queries.go
@@ -1,5 +1,13 @@
 package handlers
 
 const (
-	QueryProfileById = `SELECT id, nickname, record, win, loss FROM profile WHERE id = $1`
+	QueryProfiles                   = `SELECT nickname, record, win, loss FROM profile`
+	QueryProfilesWithLimit          = `SELECT nickname, record, win, loss FROM profile LIMIT $1`
+	QueryProfilesWithOffset         = `SELECT nickname, record, win, loss FROM profile OFFSET $1`
+	QueryProfilesWithLimitAndOffset = `SELECT nickname, record, win, loss FROM profile LIMIT $1 OFFSET $2 ORDER BY score`
+	QueryProfileById                = `SELECT nickname, record, win, loss FROM profile WHERE id = $1`
+	QueryProfileByEmail             = `SELECT email FROM profile WHERE LOWER(email) = LOWER($1)`
+	QueryProfileByNickname          = `SELECT nickname FROM profile WHERE LOWER(nickname) = LOWER($1)`
+	QueryInsertProfile              = `INSERT INTO profile (email, nickname, password) VALUES ($1, $2, $3) RETURNING id, email, nickname`
+	QueryProfileUnsafe              = `SELECT id, email, nickname, record, win, loss FROM profile WHERE id = $1`
 )
