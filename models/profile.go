36ee243c8f254a5b1f256dd9dad2ac4175e8accb
Arsenij Zorin <zorinarsenij@mail.ru>
2019-03-05 13:47:42 +0300

    [UPD] Restruct models and use easyjson

--- models/profile.go
+++ models/profile.go
@@ -2,16 +2,31 @@ package models
 
 //easyjson:json
 type Profile struct {
-	ID       uint   `json:"id"`
-	Email    string `json:"email,omitempty"`
-	Nickname string `json:"nickname"`
-	Password string `json:"password,omitempty"`
+	ID uint64 `json:"id,omitempty"`
+	ProfileInfo
 	Score
 }
 
 //easyjson:json
 type Score struct {
-	Record int `json:"record"`
-	Win    int `json:"win"`
-	Loss   int `json:"loss"`
+	Record int `json:"record,omitempty"`
+	Win    int `json:"win,omitempty"`
+	Loss   int `json:"loss,omitempty"`
 }
+
+//easyjson:json
+type ProfileInfo struct {
+	Email    string `json:"email,omitempty"`
+	Nickname string `json:"nickname,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+//easyjson:json
+type ProfileRegistration struct {
+	Email    string `json:"email"`
+	Nickname string `json:"nickname"`
+	Password string `json:"password"`
+}
+
+//easyjson:json
+type Profiles []Profile
