58b3733252beddcea1d149f4c7261ef88391d4cc
Arsenij Zorin <zorinarsenij@mail.ru>
2019-04-21 14:40:51 +0300

    [UPD] Start working with microservices

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,16 +5,17 @@ import (
 	"net/http"
 	"os"
 
-	_ "github.com/go-park-mail-ru/2019_1_The-Recovery-Team/docs"
-	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/delivery/http/rest"
-	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/infrastructure/repository/memory/game"
-	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/infrastructure/repository/postgresql"
-	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/infrastructure/repository/postgresql/profile"
-	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/infrastructure/repository/redis/session"
-	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/usecase"
+	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/delivery/http/rest/profile/api"
 
-	"github.com/gomodule/redigo/redis"
-	"github.com/jackc/pgx"
+	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/delivery/grpc/service/profile"
+
+	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/delivery/grpc/service/session"
+
+	"google.golang.org/grpc"
+
+	_ "github.com/go-park-mail-ru/2019_1_The-Recovery-Team/docs"
+	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/infrastructure/repository/memory/game"
+	"github.com/go-park-mail-ru/2019_1_The-Recovery-Team/internal/usecase"
 	httpSwagger "github.com/swaggo/http-swagger"
 	"go.uber.org/zap"
 )
@@ -32,52 +33,39 @@ func main() {
 		port = "8080"
 	}
 
-	psqlConfig := pgx.ConnConfig{
-		Host:     "db",
-		Port:     5432,
-		Database: "sadislands",
-		User:     "recoveryteam",
-		Password: "123456",
-	}
-
-	// Create connection for migrations
-	psqlConn, err := pgx.Connect(psqlConfig)
-	if err != nil {
-		log.Fatal("Postgresql connection refused")
-	}
-
-	if err := postgresql.MakeMigrations(psqlConn, "build/schema/0_initial.sql"); err != nil {
-		log.Fatal("Database migrations failed", err)
-	}
-	psqlConn.Close()
-
-	// Create new connection to database with updated OIDs
-	psqlConn, err = pgx.Connect(psqlConfig)
-	if err != nil {
-		log.Fatal("Postgresql connection refused")
-	}
-	defer psqlConn.Close()
-
-	redisConn, err := redis.DialURL("redis://:@redis:6379")
-	if err != nil {
-		log.Fatal("Redis connection refused")
-	}
-	defer redisConn.Close()
-
 	logger, err := zap.NewProduction()
 	if err != nil {
 		log.Fatal("Logger creation error")
 	}
 	defer logger.Sync()
 
-	profileInteractor := usecase.NewProfileInteractor(profile.NewProfileRepo(psqlConn))
-	sessionInterctor := usecase.NewSessionInteractor(session.NewSessionRepo(&redisConn))
+	profileConn, err := grpc.Dial(
+		"localhost:9091",
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatal("Profile service connection refused")
+	}
+	defer profileConn.Close()
+
+	sessionConn, err := grpc.Dial(
+		"localhost:8081",
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatal("Authentication service connection refused")
+	}
+	defer sessionConn.Close()
+
+	sessionManager := session.NewSessionClient(sessionConn)
+	profileManager := profile.NewProfileClient(profileConn)
+
 	gameInteractor := usecase.NewGameInteractor(game.NewGameRepo(logger))
 	go gameInteractor.Run()
 
-	api := rest.NewRestApi(profileInteractor, sessionInterctor, gameInteractor, logger)
-	api.Router.Handler("GET", "/swagger/:file", httpSwagger.WrapHandler)
-	api.Router.ServeFiles("/upload/*filepath", http.Dir("upload"))
+	profileApi := api.NewApi(&profileManager, &sessionManager, logger)
+	profileApi.Router.Handler("GET", "/swagger/:file", httpSwagger.WrapHandler)
+	profileApi.Router.ServeFiles("/upload/*filepath", http.Dir("upload"))
 
-	log.Print(http.ListenAndServe(":"+port, api.Router))
+	log.Print(http.ListenAndServe(":"+port, profileApi.Router))
 }
