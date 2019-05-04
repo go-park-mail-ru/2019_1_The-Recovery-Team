d1935befa1c86564587fc0e013779bea5c6dfaff
Arsenij Zorin <zorinarsenij@mail.ru>
2019-05-04 17:05:26 +0300

    [UPD] Scripts

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 go test -coverpkg=./... -coverprofile=coverage.out.tmp  ./...
-cat coverage.out.tmp | grep -v "_easyjson.go" > coverage.out
+cat coverage.out.tmp | grep -v -E "_easyjson.go|.pb.go|docs.go" > coverage.out
 rm coverage.out.tmp
 go tool cover -func=coverage.out
 go tool cover -html=coverage.out
