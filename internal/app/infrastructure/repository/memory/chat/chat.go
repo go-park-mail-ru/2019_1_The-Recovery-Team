02595739f02555b4271728b1f9ee88434cdc9a01
Arsenij Zorin <zorinarsenij@mail.ru>
2019-05-12 17:04:54 +0300

    [FIX] Divide large function

--- internal/app/infrastructure/repository/memory/chat/chat.go
+++ internal/app/infrastructure/repository/memory/chat/chat.go
@@ -61,110 +61,132 @@ func (c *Chat) broadcast(receiver *uint64, actionType string, payload interface{
 	})
 }
 
+func (c *Chat) initGlobalMessage(action *chat.Action) {
+	payload := action.Payload.(*chat.InitGlobalMessagesPayload)
+	query := &chat.Query{
+		Author:   payload.Author,
+		Receiver: payload.Receiver,
+		Start:    payload.Start,
+		Limit:    payload.Limit,
+	}
+	if query.Limit == 0 {
+		query.Limit = 10
+	}
+	messages, err := c.MessageManager.GetGlobal(query)
+	if err != nil {
+		c.Log.Error("Can't get global messages",
+			zap.String("error", err.Error()))
+		return
+	}
+
+	user, ok := c.Users.Load(payload.SessionID)
+	if !ok {
+		c.Log.Error("Can't find user requested global messages")
+		return
+	}
+
+	user.(*chat.User).Messages <- &chat.Action{
+		Type:    chat.SetGlobalMessages,
+		Payload: messages,
+	}
+}
+
+func (c *Chat) initMessage(action *chat.Action) {
+	payload := action.Payload.(*chat.InitMessagePayload)
+	message := &chat.Message{
+		Author:   payload.Author,
+		Receiver: payload.Receiver,
+		Data: chat.Data{
+			Text: payload.Data.Text,
+		},
+	}
+
+	created, err := c.MessageManager.Create(message)
+	if err != nil {
+		c.Log.Error("Database error",
+			zap.String("error", err.Error()))
+		return
+	}
+
+	c.broadcast(created.Receiver, chat.SetMessage, created)
+}
+
+func (c *Chat) initUpdateMessage(action *chat.Action) {
+	payload := action.Payload.(*chat.InitUpdateMessagePayload)
+	message := &chat.Message{
+		ID: payload.Id,
+		Data: chat.Data{
+			Text: payload.Data.Text,
+		},
+		Author: payload.Author,
+	}
+
+	updated, err := c.MessageManager.Update(message)
+	if err != nil {
+		c.Log.Error("Database error",
+			zap.String("error", err.Error()))
+		return
+	}
+
+	c.broadcast(updated.Receiver, chat.SetUpdateMessage, updated)
+}
+
+func (c *Chat) initPrinting(action *chat.Action) {
+	payload := action.Payload.(*chat.InitPrintingPayload)
+	c.Users.Range(func(key, value interface{}) bool {
+		if key.(string) == payload.SessionID {
+			return true
+		}
+		value.(*chat.User).Messages <- &chat.Action{
+			Type: chat.SetPrinting,
+			Payload: chat.SetPrintingPayload{
+				Id: *payload.Author,
+			},
+		}
+		return true
+	})
+}
+
+func (c *Chat) initDeleteMessage(action *chat.Action) {
+	payload := action.Payload.(*chat.InitDeleteMessagePayload)
+	message := &chat.Message{
+		ID:     payload.Id,
+		Author: payload.Author,
+	}
+
+	deleted, err := c.MessageManager.Delete(message)
+	if err != nil {
+		c.Log.Error("Database error",
+			zap.String("error", err.Error()))
+		return
+	}
+
+	c.broadcast(deleted.Receiver, chat.SetDeleteMessage, deleted)
+}
+
 func (c *Chat) processAction() {
 	for action := range c.Actions {
 		c.Log.Info("Receive message")
 		switch action.Type {
 		case chat.InitGlobalMessages:
 			{
-				payload := action.Payload.(*chat.InitGlobalMessagesPayload)
-				query := &chat.Query{
-					Author:   payload.Author,
-					Receiver: payload.Receiver,
-					Start:    payload.Start,
-					Limit:    payload.Limit,
-				}
-				if query.Limit == 0 {
-					query.Limit = 10
-				}
-				messages, err := c.MessageManager.GetGlobal(query)
-				if err != nil {
-					c.Log.Error("Can't get global messages",
-						zap.String("error", err.Error()))
-					continue
-				}
-
-				user, ok := c.Users.Load(payload.SessionID)
-				if !ok {
-					c.Log.Error("Can't find user requested global messages")
-					continue
-				}
-
-				user.(*chat.User).Messages <- &chat.Action{
-					Type:    chat.SetGlobalMessages,
-					Payload: messages,
-				}
+				c.initGlobalMessage(action)
 			}
 		case chat.InitMessage:
 			{
-				payload := action.Payload.(*chat.InitMessagePayload)
-				message := &chat.Message{
-					Author:   payload.Author,
-					Receiver: payload.Receiver,
-					Data: chat.Data{
-						Text: payload.Data.Text,
-					},
-				}
-
-				created, err := c.MessageManager.Create(message)
-				if err != nil {
-					c.Log.Error("Database error",
-						zap.String("error", err.Error()))
-					continue
-				}
-
-				c.broadcast(created.Receiver, chat.SetMessage, created)
+				c.initMessage(action)
 			}
 		case chat.InitUpdateMessage:
 			{
-				payload := action.Payload.(*chat.InitUpdateMessagePayload)
-				message := &chat.Message{
-					ID: payload.Id,
-					Data: chat.Data{
-						Text: payload.Data.Text,
-					},
-					Author: payload.Author,
-				}
-
-				updated, err := c.MessageManager.Update(message)
-				if err != nil {
-					c.Log.Error("Database error",
-						zap.String("error", err.Error()))
-					continue
-				}
-
-				c.broadcast(updated.Receiver, chat.SetUpdateMessage, updated)
+				c.initUpdateMessage(action)
 			}
 		case chat.InitPrinting:
-			payload := action.Payload.(*chat.InitPrintingPayload)
-			c.Users.Range(func(key, value interface{}) bool {
-				if key.(string) == payload.SessionID {
-					return true
-				}
-				value.(*chat.User).Messages <- &chat.Action{
-					Type: chat.SetPrinting,
-					Payload: chat.SetPrintingPayload{
-						Id: *payload.Author,
-					},
-				}
-				return true
-			})
+			{
+				c.initPrinting(action)
+			}
 		case chat.InitDeleteMessage:
 			{
-				payload := action.Payload.(*chat.InitDeleteMessagePayload)
-				message := &chat.Message{
-					ID:     payload.Id,
-					Author: payload.Author,
-				}
-
-				deleted, err := c.MessageManager.Delete(message)
-				if err != nil {
-					c.Log.Error("Database error",
-						zap.String("error", err.Error()))
-					continue
-				}
-
-				c.broadcast(deleted.Receiver, chat.SetDeleteMessage, deleted)
+				c.initDeleteMessage(action)
 			}
 		}
 	}
