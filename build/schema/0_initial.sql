adc74653dc5e082ce156f069f79a10cb8993dfef
Arsenij Zorin <zorinarsenij@mail.ru>
2019-05-13 17:44:55 +0300

    [UPD] Change default rating value

--- build/schema/0_initial.sql
+++ build/schema/0_initial.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS profile (
     nickname citext UNIQUE NOT NULL CHECK (nickname <> ''),
     avatar TEXT DEFAULT '',
 
-    record INTEGER DEFAULT 0,
+    record INTEGER DEFAULT 1500,
     win INTEGER DEFAULT 0,
     loss INTEGER DEFAULT 0
 ) WITH (autovacuum_enabled = true, autovacuum_analyze_threshold = 3);
